c6b8d073335955e3234722f14ac050790286409d
Caio <ntjrio7@gmail.com>
2024-09-18 22:48:04 -0300

    Commit novo

--- SISTEMAdeRESERVAS/src/JReservas.java
+++ SISTEMAdeRESERVAS/src/JReservas.java
@@ -1,17 +1,29 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JComboBox;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextPane;
+import javax.swing.JList;
+import javax.swing.JSeparator;
+import javax.swing.JSpinner;
+import javax.swing.JScrollBar;
+import java.awt.List;
+import java.awt.Scrollbar;
+import java.awt.Choice;
+import javax.swing.JScrollPane;
+import java.awt.ScrollPane;
 
 public class JReservas extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 
-	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -25,23 +37,48 @@ public class JReservas extends JFrame {
 		});
 	}
 
-
 	public JReservas() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 455, 301);
+		setBounds(100, 100, 614, 334);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		String [] quartos = {"Quarto 234","Quarto 454","Quarto 432","Quarto 237","Quarto 975","Quarto 886","Quarto 656","Quarto 535","Quarto 322","Quarto 121"};	
-		JComboBox <String>comboBox = new JComboBox(quartos);
-		comboBox.setBounds(137, 102, 135, 21);
-		contentPane.add(comboBox);
-		
-		JButton btnNewButton = new JButton("Reservar");
-		btnNewButton.setBounds(336, 219, 95, 35);
-		contentPane.add(btnNewButton);
-	}
 
+		contentPane.setLayout(null);
+
+		JButton btnNewButton = new JButton("Reservar");
+		btnNewButton.setBounds(503, 256, 95, 28);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+			}
+		});
+		contentPane.add(btnNewButton);
+		
+		String[] quartos = { "Quarto Deluxe com Vista para o Mar", "Suíte Presidencial", "Quarto Econômico",
+				"Quarto Familiar", "Quarto Standard", "Quarto Luxo com Terraço Privado", "Suíte Júnior",
+				"Cabana na Praia", "Quarto Temático para Crianças", "Quarto Romântico com Hidromassagem",
+				"Quarto Executivo", "Quarto Loft", "Quarto com Piscina Privativa", "Suíte de Spa",
+				"Quarto com Vista para a Montanha", "Quarto Triplo Standard", "Suíte Nupcial",
+				"Quarto com Cozinha Compacta", "Quarto de Luxo com Banho Turco", "Quarto com Varanda e Rede",
+				"Quarto com Lareira", "Suíte com Sauna Privativa", "Quarto com Vista para o Lago" };
+		JList <String> list = new JList<>(quartos);
+		
+		JScrollPane scrollPane = new JScrollPane(list);
+		scrollPane.setBounds(66, 34, 511, 194);
+		contentPane.add(scrollPane);
+		
+		
+        
+       
+		
+		
+		
+		
+		
+		
+		
+
+	}
 }
