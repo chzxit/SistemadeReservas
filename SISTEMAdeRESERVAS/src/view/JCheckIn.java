9a3dfd07c8e87225313d617cf900a3aed4cc8c47
Caio <ntjrio7@gmail.com>
2024-09-22 16:22:58 -0300

    Commit atualizaçoes 2.0

---
+++ SISTEMAdeRESERVAS/src/view/JCheckIn.java
@@ -0,0 +1,62 @@
+package view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+
+public class JCheckIn extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JCheckIn frame = new JCheckIn();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public JCheckIn() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 470, 312);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel(" Check-in");
+		lblNewLabel.setFont(new Font("Arial", Font.BOLD | Font.ITALIC, 18));
+		lblNewLabel.setBounds(185, 11, 110, 31);
+		contentPane.add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("Cadastrar Hóspede");
+		btnNewButton.setBounds(10, 79, 157, 48);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Quartos");
+		btnNewButton_1.setBounds(297, 79, 157, 48);
+		contentPane.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Resgistrar Check-in");
+		btnNewButton_2.setBounds(297, 231, 147, 31);
+		contentPane.add(btnNewButton_2);
+	}
+}
---
+++ SISTEMAdeRESERVAS/src/view/JCheckOut.java
@@ -0,0 +1,59 @@
+package view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+
+public class JCheckOut extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JCheckOut frame = new JCheckOut();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public JCheckOut() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 385, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Check-out");
+		lblNewLabel.setFont(new Font("Arial", Font.BOLD | Font.ITALIC, 18));
+		lblNewLabel.setBounds(143, 11, 112, 25);
+		contentPane.add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("Registrar Check-out");
+		btnNewButton.setBounds(197, 211, 162, 39);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Despesas");
+		btnNewButton_1.setBounds(10, 211, 112, 39);
+		contentPane.add(btnNewButton_1);
+	}
+
+}
